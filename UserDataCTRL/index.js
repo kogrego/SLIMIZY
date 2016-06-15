564211f9f9ad1963c284ace977e3ab5ebc26d50a
kogrego <kogrego@gmail.com>
2016-06-15 21:33:13 +0300

    Update index.js
    changed from class to function export

--- UserDataCTRL/index.js
+++ UserDataCTRL/index.js
@@ -1,38 +1,40 @@
-'use strict';
 
-var EventEmitter = require('events').EventEmitter,
-    express = require('express'),
-    eventsConfig = require('./config').events;
+var userDataSchema = require('./user_schema');
 
-class userData extends EventEmitter {
-	constructor(data){
-		super();
-		this.data = data;
-		this.json = null;
-		
-		this.on(eventsConfig.GETUSERBYID, (id) => {
-			console.log('on getById: ' + id);
-			this.json = data;
+exports.getUserById = (req, res) => {
+	var query = userDataSchema.findOne({id:req.params.id});
+  	query.exec((err, data) => {
+		if (err) res.send(err);
+		res.status(200).json(data);
+	});
+};
+
+exports.updateUserBMI = (req, res) => {
+	var BMI = 
+		{
+			gender: req.body.gender,
+			weight: req.body.weight,
+			height: req.body.height,
+			BMIScore: req.body.BMIScore
+		};
+  	var query = userDataSchema.findOne({id:req.params.id});
+  	query.exec((err, data) => {
+		if (err) res.send(err);
+		if (BMI.gender != null) data.BMI.gender = BMI.gender;
+		if (BMI.gender != null) data.BMI.weight = BMI.weight;
+		if (BMI.gender != null) data.BMI.height = BMI.height;
+		if (BMI.gender != null) data.BMI.BMIScore = BMI.BMIScore;
+		query = data.save((err) => {
+			if (err) res.send(err);
+			else res.status(200).json({"result":"BMI for user " + data.fullName + " was updated"});	
 		});
+	});	
+};
 
-		this.on(eventsConfig.UPDATEUSERBMI, (BMI) => {
-			console.log('on updateBMI:\n' + BMI.gender + '\n' + BMI.height + '\n' + BMI.weight + '\n' + BMI.BMIScore + '\n');
-			if (BMI.gender != null) data.BMI.gender = BMI.gender;
-			if (BMI.gender != null) data.BMI.weight = BMI.weight;
-			if (BMI.gender != null) data.BMI.height = BMI.height;
-			if (BMI.gender != null) data.BMI.BMIScore = BMI.BMIScore;
-			this.json = data;
-		});
-	}
-
-	getUserById(id) {
-       	this.emit(eventsConfig.GETUSERBYID, id);
-       	return this.json;
-    }
-
-    updateUserBMI(BMI){
-    	this.emit(eventsConfig.UPDATEUSERBMI, BMI);
-    	return this.json;
-    }
-}
-module.exports = userData;
+exports.deleteUser = (req, res) => {
+	var query = userDataSchema.findOne({id:req.params.id}).remove();
+  	query.exec((err) => {
+  		if (err) res.send(err);
+		else res.status(200).json({"result":"user deleted"});
+  	});	
+};

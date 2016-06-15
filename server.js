852a923fb0241fac4358ebf6358e180521ea49e9
kogrego <kogrego@gmail.com>
2016-06-15 21:41:48 +0300

    Update server.js
    added UserData module functions

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var express = require('express'),
     router = express.Router();
  	userData = require('./UserDataCTRL'),
  	foods = require('./Foods'),
- 	userDataSchema = require('./user_schema'),
- 	users_schema = require('./users_schema'),
  	jsonData = null,
  	login = null,
  	UserData = null,
@@ -41,16 +39,11 @@ app.use(function(req,res,next){
     next();
 });
 //Routs & Callback functions:
-app.get('/', (req, res, next)=>{
-	res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res, next)=>{
+// 	res.sendFile(__dirname + '/index.html');
+// });
 
-app.get('/user/:id', (req, res) => {
-  	console.log('getUserById');
-  	dbConnect.on('error', console.error.bind(console, 'connection error:'));
-    jsonData = UserData.getUserById(req.params.id); 
-	res.status(200).json(jsonData);
-});
+app.get('/user/:id', userData.getUserById);
 
 app.get('/recipes/:calories/:searchTerm', (req, res) =>{
 	// Foods = new foods();
@@ -64,43 +57,13 @@ app.get('/login/cal4today/:id/:d/:m/:y', userLogin.cal4today);
 
 app.get('/login/userProfile/:id', userLogin.userProfile);
 
-app.get('/user/BMI/:id', (req, res) => {
-  	console.log('updateUser');
-  	var gender = req.param.gender;
-  	var weight = req.param.weight;
-  	var height = req.param.height;
-  	var BMIScore = req.param.BMIScore;
-  	var query = userDataSchema.find({}).where('id').equals(req.params.id);
-  	query.exec((err, data) => {
-		if (err) res.send(err);
-		UserData = new userData(data);
-		console.log('userData created' + data);
-		var BMI = {
-			gender: gender,
-			weight: weight,
-			height: height,
-			BMIScore: BMIScore
-		}
-		jsonData = UserData.updateUserBMI(BMI);
-		userDataSchema.update({},(err, id) => {
-			if (err) res.send(err);
-			res.status(200).json(data);
-		});
-	}); 
-});
-
+app.put('/user/BMI/:id', userData.updateUserBMI);
 
 app.post('/register', (req, res) => {
 	console.log('register');
 });
 
-app.put('/user/:id', (req, res) => {
-  	console.log('updateUser'); 
-});
-
-app.delete('/user/:id', (req, res) => {
-  	console.log('deleteUser');
-});
+app.delete('/user/:id', userData.deleteUser);
 
 app.listen(port, () => {
  	console.log('listening on port: ' + port);

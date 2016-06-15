117269bcd36e4fb3d28a37f9c1e55935f3fabb3c
davidAvigad <david.avigad@gmail.com>
2016-06-16 01:39:29 +0300

    last update

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,6 +1,6 @@
 var loginMain = '<div class="form-container" ng-controller="loginCTRL">'+
                 '<h2>Login</h2>'+
-                '<form method="post" action="http://localhost:3000/loginAuth">'+
+                '<form method="post" action="/loginAuth">'+
                     '<div class="form-group">'+
                         '<label for="username">ID</label>'+
                         '<input type="text" name="id" id="id" value="" class="form-control">'+
